2325690778bf8d54f62d788883edb974b5583ad4
Martín (Netux) Rodriguez <send.email@netux.site>
2025-04-13 18:35:54 -0300

    feat: add arrow before ellipsis

--- ViewCurrentPath.cs
+++ ViewCurrentPath.cs
@@ -150,7 +150,7 @@ public class EscapeMenuCurrentPath : MonoBehaviour
         var result = string.Join(" → ", aggregatedQueuedNodeTypes);
         if (hasMore)
         {
-            result += " …";
+            result += " → …";
         }
         return result;
     }
