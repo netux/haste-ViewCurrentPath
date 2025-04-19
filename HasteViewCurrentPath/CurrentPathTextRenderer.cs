907eb5a873b78b30ee28ec795873c6bb889f3ec1
Martín (Netux) Rodriguez <send.email@netux.site>
2025-04-18 23:11:10 -0300

    style(text renderer): remove debug logs
    This seems to work fine now, with the failsafe

--- HasteViewCurrentPath/CurrentPathTextRenderer.cs
+++ HasteViewCurrentPath/CurrentPathTextRenderer.cs
@@ -9,39 +9,32 @@ public class CurrentPathTextRenderer : ICurrentPathRenderers
 
     public void Setup(RectTransform parentTransform)
     {
-        if (text != null)
+        if (text == null)
         {
-            Debug.Log($"CurrentPathTextRenderer Setup: text was not null");
-            return;
+            text = new GameObject("Text", [typeof(TextMeshProUGUI)]).GetComponent<TextMeshProUGUI>();
+            text.gameObject.transform.SetParent(parentTransform, worldPositionStays: false);
+
+            var referenceText = Util.GetReferenceTMP(parentTransform.parent);
+
+            text.font = referenceText.font;
+            text.fontMaterial = referenceText.fontMaterial;
+            text.fontSharedMaterial = referenceText.fontSharedMaterial;
+            text.enableWordWrapping = false;
+            text.overflowMode = TextOverflowModes.Overflow;
+            text.verticalAlignment = VerticalAlignmentOptions.Middle;
+
+            var textTransform = (RectTransform)text.gameObject.transform;
+            textTransform.localScale = Vector3.one;
+            textTransform.position = Vector3.zero;
+            textTransform.localPosition = Vector3.zero;
+            textTransform.pivot = Vector2.one / 2;
+            textTransform.anchorMin = Vector2.zero;
+            textTransform.anchorMax = Vector2.one;
+            textTransform.sizeDelta = Vector2.zero;
         }
-
-        text = new GameObject("Text", [typeof(TextMeshProUGUI)]).GetComponent<TextMeshProUGUI>();
-        text.gameObject.transform.SetParent(parentTransform, worldPositionStays: false);
-
-        var referenceText = parentTransform.Find("../CancelPath/Text").GetComponent<TextMeshProUGUI>();
-
-        text.font = referenceText.font;
-        text.fontMaterial = referenceText.fontMaterial;
-        text.fontSharedMaterial = referenceText.fontSharedMaterial;
-        text.enableWordWrapping = false;
-        text.overflowMode = TextOverflowModes.Overflow;
-        text.verticalAlignment = VerticalAlignmentOptions.Middle;
-
-        var textTransform = (RectTransform)text.gameObject.transform;
-        textTransform.localScale = Vector3.one;
-        textTransform.position = Vector3.zero;
-        textTransform.localPosition = Vector3.zero;
-        textTransform.pivot = Vector2.one / 2;
-        textTransform.anchorMin = Vector2.zero;
-        textTransform.anchorMax = Vector2.one;
-        textTransform.sizeDelta = Vector2.zero;
     }
 
-    public bool CanBeUsed() {
-        Debug.Log($"CurrentPathTextRenderer CanBeUsed. text == null? {text == null}");
-
-        return text != null;
-    }
+    public bool CanBeUsed() => text != null;
 
     public void Render(IEnumerable<PathAggregator.PathNode> nodes, bool showEllipsis)
     {
@@ -88,7 +81,7 @@ public class CurrentPathTextRenderer : ICurrentPathRenderers
             LevelSelectionNode.NodeType.Encounter => "Encounter",
             LevelSelectionNode.NodeType.RestStop => "Rest",
             LevelSelectionNode.NodeType.Boss => "Boss",
-            _ => nodeType.ToString(),
+            _ => nodeType.ToString(), // Shouldn't happen as long as Landfall doesn't add a new node type...
         };
     }
 }
